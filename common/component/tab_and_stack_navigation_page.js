c79d546b83b0e6ddff91e26bfd00c5a5da25d788
dreamflylin <dreamflylin@gmail.com>
2017-07-30 10:30:57 +0800

    add tab_and_stack_navigation_page.js

---
+++ common/component/tab_and_stack_navigation_page.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, Button, AppRegistry } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+
+class ChatScreen extends Component {
+  static navigationOptions = ({navigation}) => ({
+    title: `Chat with ${navigation.state.params.user}`,
+  });
+
+  render() {
+    const { params } = this.props.navigation.state;
+    return (
+      <View>
+        <Text>Chat with {params.user} </Text>
+      </View>
+    );
+  }
+}
+
+class RecentChatsScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>List of recent chats</Text>
+        <Button
+          onPress={() => this.props.navigation.navigate('Chat', {user: 'Lucy'})}
+          title='Chat with Lucy'
+        />
+      </View>
+    );
+  }
+}
+
+class AllChatsScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>List of all contacts</Text>
+        <Button
+          onPress={() => this.props.navigation.navigate('Chat', {user: 'Lucy'})}
+          title='Chat with Lucy'
+        />
+      </View>
+    );
+  }
+}
+
+const TabScreenNavigator = TabNavigator({
+  Recent: {screen: RecentChatsScreen},
+  All: {screen: AllChatsScreen},
+});
+
+TabScreenNavigator.navigationOptions = {
+  title: 'My Chats',
+};
+
+export const MainPage = StackNavigator({
+  Home: {screen: TabScreenNavigator},
+  Chat: {screen: ChatScreen},
+});
+
+export default MainPage;
